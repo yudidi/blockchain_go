b81a4851833c1b2276a33d9f63a9448f9ef48627
yudidi <972656027@qq.com>
2019-01-29 22:04:18 +0800

    添加注释

--- block.go
+++ block.go
@@ -9,16 +9,19 @@ type Block struct {
 	Timestamp     int64
 	Data          []byte
 	PrevBlockHash []byte
-	Hash          []byte
-	Nonce         int
+	//todo 共识属性
+	Hash  []byte
+	Nonce int
 }
 
+// todo 使用pow把仅有数据属性的区块，加工为一个拥有完备属性(共识属性)的合法区块。
 // NewBlock creates and returns Block
 func NewBlock(data string, prevBlockHash []byte) *Block {
 	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}, 0}
+	// todo block：打包好的区块，进行记账权的争夺。挖矿
 	pow := NewProofOfWork(block)
 	nonce, hash := pow.Run()
-
+	//todo 通过pow，暴力尝试出一个nonce值，把nonce和对应的hash放入块中，使得其他人可以验证，
 	block.Hash = hash[:]
 	block.Nonce = nonce
 
--- proofofwork.go
+++ proofofwork.go
@@ -6,17 +6,20 @@ import (
 	"fmt"
 	"math"
 	"math/big"
+	"time"
 )
 
 var (
 	maxNonce = math.MaxInt64
 )
 
-const targetBits = 24
+const targetBits = 8 //24
 
-//todo 传入一个区块
-// （已有3个属性值【数据data，时间戳timestamp，副区块prehash】
-// 需要计算2个属性值：然后由pow暴力计算一个nonce及满足条件的hash
+//todo 一个区块共5个属性
+// 1. 传入一个区块(含3个属性)，然后计算出剩下的2个属性。
+// 已有3个属性值【数据data，时间戳timestamp，副区块prehash】
+// 计算2个属性值：然后由pow暴力计算一个nonce及满足条件的hash
+// 2. 参与hash计算的属性是3个 + 难度值和nonce值
 
 // ProofOfWork represents a proof-of-work
 type ProofOfWork struct {
@@ -34,6 +37,7 @@ func NewProofOfWork(b *Block) *ProofOfWork {
 	return pow
 }
 
+// todo 参与hash计算的数据有：
 func (pow *ProofOfWork) prepareData(nonce int) []byte {
 	data := bytes.Join(
 		[][]byte{
@@ -49,10 +53,11 @@ func (pow *ProofOfWork) prepareData(nonce int) []byte {
 	return data
 }
 
+// todo 切片到定义和初始化 http://www.runoob.com/go/go-slice.html
 // Run performs a proof-of-work
 func (pow *ProofOfWork) Run() (int, []byte) {
 	var hashInt big.Int
-	var hash [32]byte
+	var hash [32]byte //todo 你可以声明一个未指定大小的数组来定义切片：
 	nonce := 0
 
 	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
@@ -60,10 +65,13 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 		data := pow.prepareData(nonce)
 
 		hash = sha256.Sum256(data)
-		fmt.Printf("\r%x", hash)
+		fmt.Printf("当前得到的hash %x \r", hash)
+		fmt.Printf("此时的nonce值 %v \r ", nonce)
 		hashInt.SetBytes(hash[:])
 
 		if hashInt.Cmp(pow.target) == -1 {
+			fmt.Printf("找到符合条件的hash值 %x", hash)
+			time.Sleep(1000)
 			break
 		} else {
 			nonce++
@@ -71,7 +79,7 @@ func (pow *ProofOfWork) Run() (int, []byte) {
 	}
 	fmt.Print("\n\n")
 
-	return nonce, hash[:]
+	return nonce, hash[:] //todo 返回切片的第一个元素到最后一个元素(所有元素)
 }
 
 // Validate validates block's PoW
