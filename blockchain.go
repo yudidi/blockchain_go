738071753bf9107afe33053fe1f73c4cd0d93267
yudidi <972656027@qq.com>
2019-01-31 23:19:07 +0800

    add comments.

--- blockchain.go
+++ blockchain.go
@@ -12,6 +12,7 @@ const blocksBucket = "blocks"
 
 // Blockchain keeps a sequence of Blocks
 type Blockchain struct {
+	// todo tip 有尾部，尖端的意思，在这里 tip 存储的是最后一个块的哈希）
 	tip []byte
 	db  *bolt.DB
 }
@@ -85,6 +86,9 @@ func (i *BlockchainIterator) Next() *Block {
 	return block
 }
 
+//todo 区块链存放在数据库的bucket这种数据结构中，可以存放多个k-v对，每个k-v对表示一个区块
+// 为了从数据库文件中获取一个区块，需要知道bucket和key，才能拿到value
+
 // NewBlockchain creates a new Blockchain with genesis Block
 func NewBlockchain() *Blockchain {
 	var tip []byte
@@ -93,30 +97,30 @@ func NewBlockchain() *Blockchain {
 		log.Panic(err)
 	}
 
+	// todo 创建/读取 一个区块链并返回。 需要设置/读取 Blockchain的2个属性：db 和 tip
 	err = db.Update(func(tx *bolt.Tx) error {
 		b := tx.Bucket([]byte(blocksBucket))
-
 		if b == nil {
 			fmt.Println("No existing blockchain found. Creating a new one...")
 			genesis := NewGenesisBlock()
-
-			b, err := tx.CreateBucket([]byte(blocksBucket))
+			// 1. 设置db属性
+			b, err := tx.CreateBucket([]byte(blocksBucket)) //ydd 创建一个key=blocks的键直对
 			if err != nil {
 				log.Panic(err)
 			}
 
-			err = b.Put(genesis.Hash, genesis.Serialize())
+			err = b.Put(genesis.Hash, genesis.Serialize()) // ydd 放入创世区块的hash -> 序列化之后的块
 			if err != nil {
 				log.Panic(err)
 			}
-
-			err = b.Put([]byte("l"), genesis.Hash)
+			// 2。 设置tip属性
+			err = b.Put([]byte("l"), genesis.Hash) //
 			if err != nil {
 				log.Panic(err)
 			}
 			tip = genesis.Hash
 		} else {
-			tip = b.Get([]byte("l"))
+			tip = b.Get([]byte("l")) //ydd 获取最后一个块的hash
 		}
 
 		return nil
